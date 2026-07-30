5a6f727e4f486d45a92749e57e4d3825c5812852
michaelnorton <michael.norton1213@gmail.com>
2026-07-29 20:13:10 -0400

    expose port

--- main.go
+++ main.go
@@ -3,16 +3,21 @@ package main
 import (
 	"fmt"
 	handler "go_text/api"
+	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
+	http.HandleFunc("/", handler.Handler)
 	http.HandleFunc("/api/health", handler.Handler)
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
+
+	addr := "0.0.0.0:" + port
 	fmt.Printf("listening on port %s...\n", port)
 	http.ListenAndServe(":8080", nil)
+	log.Fatal(http.ListenAndServe(addr, nil))
 }
